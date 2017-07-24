57e9d201b5ee3375d15b360279fd01e5d2542f75
jayherring <j.herring.8989@gmail.com>
2017-07-24 17:45:42 -0500

    locationlist and current

---
+++ LocationList.js
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var LocationItem = require(./LocationItem);
+
+var LocationList = React.createClass({
+
+	render(){
+
+		var self = this;
+
+		var locations = this.props.locations.map(function(1){
+
+			var active = self.pops.activeLocationAddress == 1.address;
+
+			//passing onClick callback of his LocationList to each Item.
+
+			return <LocationItem address={1.address} timestamp={1.timestamp}
+						active={active} onClick={self.props.onClick} />
+		});
+
+	if(!locations.length){
+		return null;
+	}
+
+	return (
+		<div className
+	)
+
+	}
+})
